272d0c8c555f44155eaa78a15fd4c176a7ab7395
Jlehub <elloh347@gmail.com>
2024-11-18 00:25:08 -0500

    second commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,11 +21,15 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+	
+
 }
 
 kotlin {
---
+++ src/main/kotlin/seg3x02/tempconverterapi/controller/WebSecurityConfig.kt
@@ -0,0 +1,58 @@
+package seg3x02.tempconverterapi.controller
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.config.http.SessionCreationPolicy
+
+
+@Configuration
+class WebSecurityConfig {
+
+    @Bean
+    @Throws(Exception::class)
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { auth -> auth.anyRequest().authenticated() }
+            .httpBasic { httpBasic -> httpBasic.realmName("Your Application Realm") }
+            .csrf { csrf -> csrf.disable() }
+            .sessionManagement { session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            
+            
+            
+            
+        return http.build()
+    }
+
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val user1: UserDetails = User.withUsername("user1")
+            .password(passwordEncoder().encode("pass1"))
+            .roles("USER")
+            .build()
+
+        val user2: UserDetails = User.withUsername("user2")
+            .password(passwordEncoder().encode("pass2"))
+            .roles("USER")
+            .build()
+
+        return InMemoryUserDetailsManager(user1, user2)
+    }
+
+    @Bean
+    fun passwordEncoder(): BCryptPasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+}
